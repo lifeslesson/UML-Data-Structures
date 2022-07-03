a5a724e36c587754e52d4938f82e275fac0096f7
lifeslesson <28712513+lifeslesson@users.noreply.github.com>
2022-07-03 19:24:09 -0400

    initial upload
    non-edited program

---
+++ Using_Queues_Program_5/prog5main.cpp
@@ -0,0 +1,45 @@
+/*Nathan Dynice
+   Dr. M Geiger
+   Data Structured summer 2022 */
+
+   /***************************************************************
+    * I BELIEVE YOU'LL NEED TO INCLUDE ALL OF THESE HEADER FILES; *
+    *   FEEL FREE TO MODIFY THIS LIST AS NEEDED                   *
+    ***************************************************************/
+#include "Queue.h"
+#include "QItem.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    /****************************************************************
+     * VARIABLE DECLARATIONS SHOULD INCLUDE (BUT NOT BE LIMITED TO):
+     *   - VARIABLES TO READ FROM THE FILE (INPUT STREAM, STRING FOR NAME,
+     *       ETC.)
+     *   - TWO QUEUES REPRESENTING THE TWO LANES YOU'RE MODELING
+     *   - AT LEAST ONE TEMPORARY QItem VARIABLE TO STORE USER INPUT
+     *       BEFORE ADDING IT TO EACH "LANE"
+     *       (YOU MAY WANT A SECOND TEMP VARIABLE SO YOU CAN USE THE
+     *        TEMP VARIABLES TO REPRESENT THE CUSTOMERS YOU'RE
+     *        SERVING, AND HAVE THE QUEUES JUST HOLD WAITING CUSTOMERS)
+     *   - A VARIABLE TO COUNT TIME
+     *   - SOMETHING TO TRACK (1) WHETHER YOU'RE SERVING A CUSTOMER FROM
+     *        EACH QUEUE AND (2) HOW LONG THAT CUSTOMER HAS LEFT
+     * ***************************************************************/
+
+     /*****************************************************************
+      * START WITH FILE HANDLING AS DESCRIBED IN THE SPEC
+      *   (THE TEST CASES ARE DESIGNED SO YOU CAN TEST THE FILE HANDLING
+      *    WITHOUT IMPLEMENTING ANYTHING ELSE IN THE REST OF THE PROGRAM!)
+      * ***************************************************************/
+
+      /*****************************************************************
+       * THEN, ADD THE LOOP THAT TRACKS TIME AND HANDLES CUSTOMER SERVICE
+       * ***************************************************************/
+
+    return 0;
+}
